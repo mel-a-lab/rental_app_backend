5c609c47e4b7854ab587d6d3faec99e15bc60bc0
mel-a-lab <melanie.dussenne@gmail.com>
2024-01-15 21:36:20 +0100

    correction des erreurs des entités

--- src/main/java/com/rental/entities/rental.java
+++ src/main/java/com/rental/entities/rental.java
@@ -1,22 +1,95 @@
 package com.rental.entities;
 
+import java.util.List;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
 @Entity
 public class Rental {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-  private String name;
-  private Double surface;
-  private Double price;
-  private String picture;
-  private String description;
+    private String name;
+    private Double surface;
+    private Double price;
+    private String picture;
+    private String description;
 
-  @ManyToOne
-  private User owner;
+    @ManyToOne
+    private User owner;
 
-  @OneToMany(mappedBy = "rental")
-  private List<Message> messages;
+    @OneToMany(mappedBy = "rental")
+    private List<Message> messages;
 
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getSurface() {
+        return surface;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurface(Double surface) {
+        this.surface = surface;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
 }
--- src/main/java/com/rental/entities/user.java
+++ src/main/java/com/rental/entities/user.java
@@ -1,26 +1,107 @@
 package com.rental.entities;
 
+import java.sql.Timestamp;
+import java.util.List;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+
 @Entity
 public class User {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-  private String email;
-  private String name;
-  private String password;
-  private Timestamp createdAt;
-  private Timestamp updatedAt;
+    private String email;
+    private String name;
+    private String password;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
 
-  @OneToMany(mappedBy = "owner")
-  private List<Rental> rentals;
+    @OneToMany(mappedBy = "owner")
+    private List<Rental> rentals;
 
-  @OneToMany(mappedBy = "sender")
-  private List<Message> sentMessages;
+    @OneToMany(mappedBy = "sender")
+    private List<Message> sentMessages;
 
-  @ManyToMany(mappedBy = "recipients")
-  private List<Message> receivedMessages;
+    @ManyToMany(mappedBy = "recipients")
+    private List<Message> receivedMessages;
 
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public List<Rental> getRentals() {
+        return rentals;
+    }
+
+    public List<Message> getSentMessages() {
+        return sentMessages;
+    }
+
+    public List<Message> getReceivedMessages() {
+        return receivedMessages;
+    }
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public void setRentals(List<Rental> rentals) {
+        this.rentals = rentals;
+    }
+
+    public void setSentMessages(List<Message> sentMessages) {
+        this.sentMessages = sentMessages;
+    }
+
+    public void setReceivedMessages(List<Message> receivedMessages) {
+        this.receivedMessages = receivedMessages;
+    }
 }
-
