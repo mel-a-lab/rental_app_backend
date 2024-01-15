7437848e5acd6f0cda3f7d93999c2691a452d6ca
mel-a-lab <melanie.dussenne@gmail.com>
2024-01-15 21:37:54 +0100

    correction erreur dans repository

--- src/main/java/com/rental/repository/MessageRepository.java
+++ src/main/java/com/rental/repository/MessageRepository.java
@@ -1,5 +1,14 @@
 package com.rental.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rental.entities.Message;
+import com.rental.entities.Rental;
+import com.rental.entities.User;
+
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
